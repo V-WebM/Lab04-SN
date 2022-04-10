d15576b3607fd84502c1300eb8975eda75def3e6
Ventura <marlenp363@gmail.com>
2022-04-10 14:30:26 -0500

    commit 2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const res = require('express/lib/response');
 
 const app = express();
 
-const port = 3000;
+const port = 5000;
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
